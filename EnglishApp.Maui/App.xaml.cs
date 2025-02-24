3ac84c0587fef6e7df939b0405705b644d14d91b
fnifh <fnifh@JUPITER4823>
2025-02-24 18:07:53 +0900

    プロジェクト ファイルを追加します。

---
+++ EnglishApp.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EnglishApp.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
