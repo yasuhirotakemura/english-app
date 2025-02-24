a39fc5d737998d86d236e589252e31c3a52611c0
YasuhiroTakemura <fnifhubi85h29bddi@gmail.com>
2025-02-24 20:39:08 +0900

    feat: Entityの追加

---
+++ EnglishApp.Domain/Entities/EnglishQuestionEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnglishApp.Domain.Entities;
+
+public sealed class EnglishQuestionEntity(int id,
+                                          int englishTextId,
+                                          string questionText,
+                                          string answer,
+                                          DateTime createdAt,
+                                          DateTime updatedAt)
+{
+    public int Id = id;
+    public int EnglishTextId = englishTextId;
+    public string QuestionText = questionText;
+    public string Answer = answer;
+    public DateTime CreatedAt = createdAt;
+    public DateTime UpdatedAt = updatedAt;
+}
---
+++ EnglishApp.Domain/Entities/EnglishTextEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace EnglishApp.Domain.Entities;
+
+public sealed class EnglishTextEntity(int id,
+                                string title,
+                                string content,
+                                DateTime createdAt,
+                                DateTime updatedAt)
+{
+    public int Id = id;
+    public string Title = title;
+    public string Content = content;
+    public DateTime CreatedAt = createdAt;
+    public DateTime UpdatedAt = updatedAt;
+}
