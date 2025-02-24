1d5ca51c8b5b3cb854469a20825fd8dea6d9f00b
YasuhiroTakemura <fnifhubi85h29bddi@gmail.com>
2025-02-25 07:25:24 +0900

    feat: Domainで定義したServiceInterfaceの実装

---
+++ EnglishApp.Infrastructure/Services/RepositoryService.cs
@@ -0,0 +1,22 @@
+﻿using EnglishApp.Domain.Repositories;
+using EnglishApp.Domain.Services;
+using EnglishApp.Infrastructure.Repositories;
+
+namespace EnglishApp.Infrastructure.Services;
+
+public sealed class RepositoryService(SqlServerService sqlServer) : IRepositoryService
+{
+    private readonly SqlServerService _sqlServer = sqlServer;
+
+    private IEnglishTextRepository? _englishTextRepository;
+    public IEnglishTextRepository EnglishTextRepository()
+    {
+        return this._englishTextRepository ??= new EnglishTextRepository(this._sqlServer);
+    }
+
+    private IEnglishQuestionRepository? _englishQuestionRepository;
+    public IEnglishQuestionRepository EnglishQuestionRepository()
+    {
+        return this._englishQuestionRepository ??= new EnglishQuestionRepository(this._sqlServer);
+    }
+}
