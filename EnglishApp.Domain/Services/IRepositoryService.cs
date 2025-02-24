e68a03043326ef766db66c7e6fcf52987f415019
YasuhiroTakemura <fnifhubi85h29bddi@gmail.com>
2025-02-25 07:24:51 +0900

    feat: DomainにRepositoryServiceのインターフェースを作成

---
+++ EnglishApp.Domain/Services/IRepositoryService.cs
@@ -0,0 +1,10 @@
+﻿using EnglishApp.Domain.Repositories;
+
+namespace EnglishApp.Domain.Services;
+
+public interface IRepositoryService
+{
+    public IEnglishTextRepository EnglishTextRepository();
+
+    public IEnglishQuestionRepository EnglishQuestionRepository();
+}
