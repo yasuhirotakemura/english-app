b4193b119a509a4a6913e834b48169ae8dc3bfeb
YasuhiroTakemura <fnifhubi85h29bddi@gmail.com>
2025-02-25 07:25:00 +0900

    feat: Factoryの追加

---
+++ EnglishApp.Infrastructure/Factories/EntityFactory.cs
@@ -0,0 +1,32 @@
+﻿using EnglishApp.Domain.Entities;
+using Microsoft.Data.SqlClient;
+
+namespace EnglishApp.Infrastructure.Factories;
+
+internal static class EntityFactory
+{
+    public static EnglishTextEntity CreateEnglishText(SqlDataReader reader)
+    {
+        return new
+        (
+            reader.GetInt32(reader.GetOrdinal("Id")),
+            reader.GetString(reader.GetOrdinal("Title")),
+            reader.GetString(reader.GetOrdinal("Content")),
+            reader.GetDateTime(reader.GetOrdinal("CreatedAt")),
+            reader.GetDateTime(reader.GetOrdinal("UpdatedAt"))
+        );
+    }
+
+    public static EnglishQuestionEntity CreateEnglishQuestion(SqlDataReader reader)
+    {
+        return new
+        (
+            reader.GetInt32(reader.GetOrdinal("Id")),
+            reader.GetInt32(reader.GetOrdinal("EnglishTextId")),
+            reader.GetString(reader.GetOrdinal("QuestionText")),
+            reader.GetString(reader.GetOrdinal("Answer")),
+            reader.GetDateTime(reader.GetOrdinal("CreatedAt")),
+            reader.GetDateTime(reader.GetOrdinal("UpdatedAt"))
+        );
+    }
+}
