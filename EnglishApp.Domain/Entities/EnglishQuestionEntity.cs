95de2c7e3b72fe8efe8fd098024ceb9126c0c63e
YasuhiroTakemura <fnifhubi85h29bddi@gmail.com>
2025-02-25 07:23:31 +0900

    feat: 必要なEntityの作成

--- EnglishApp.Domain/Entities/EnglishQuestionEntity.cs
+++ EnglishApp.Domain/Entities/EnglishQuestionEntity.cs
@@ -1,22 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace EnglishApp.Domain.Entities;
 
-namespace EnglishApp.Domain.Entities;
-
-public sealed class EnglishQuestionEntity(int id,
-                                          int englishTextId,
-                                          string questionText,
-                                          string answer,
-                                          DateTime createdAt,
-                                          DateTime updatedAt)
+public sealed class EnglishQuestionEntity(int id, int englishTextId, string questionText, string answer, DateTime createdAt, DateTime updatedAt)
 {
-    public int Id = id;
-    public int EnglishTextId = englishTextId;
-    public string QuestionText = questionText;
-    public string Answer = answer;
-    public DateTime CreatedAt = createdAt;
-    public DateTime UpdatedAt = updatedAt;
+    public int Id { get; set; } = id;
+    public int EnglishTextId { get; set; } = englishTextId;
+    public string QuestionText { get; set; } = questionText;
+    public string Answer { get; set; } = answer;
+    public DateTime CreatedAt { get; set; } = createdAt;
+    public DateTime UpdatedAt { get; set; } = updatedAt;
 }
+
--- EnglishApp.Domain/Entities/EnglishTextEntity.cs
+++ EnglishApp.Domain/Entities/EnglishTextEntity.cs
@@ -1,14 +1,10 @@
 ﻿namespace EnglishApp.Domain.Entities;
 
-public sealed class EnglishTextEntity(int id,
-                                string title,
-                                string content,
-                                DateTime createdAt,
-                                DateTime updatedAt)
+public sealed class EnglishTextEntity(int id, string title, string content, DateTime createdAt, DateTime updatedAt)
 {
-    public int Id = id;
-    public string Title = title;
-    public string Content = content;
-    public DateTime CreatedAt = createdAt;
-    public DateTime UpdatedAt = updatedAt;
+    public int Id { get; set; } = id;
+    public string Title { get; set; } = title;
+    public string Content { get; set; } = content;
+    public DateTime CreatedAt { get; set; } = createdAt;
+    public DateTime UpdatedAt { get; set; } = updatedAt;
 }
