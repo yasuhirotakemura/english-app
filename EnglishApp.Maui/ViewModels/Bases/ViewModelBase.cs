df7c2b34707fcffcc7b6545cbcb58e26a48281f6
YasuhiroTakemura <fnifhubi85h29bddi@gmail.com>
2025-03-01 18:08:12 +0900

    feat: ViewModelBaseの実装

---
+++ EnglishApp.Maui/ViewModels/Bases/ViewModelBase.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace EnglishApp.Maui.ViewModels.Bases;
+
+public abstract class ViewModelBase : ObservableObject
+{
+    public virtual Task NavigateToAsync(string route, object parameter = null)
+    {
+        return Shell.Current.GoToAsync(route, parameter != null ? new Dictionary<string, object> { { "parameter", parameter } } : null);
+    }
+}
