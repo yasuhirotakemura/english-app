fd70d99c81bacd52f6de7b99f146b8f118e275a5
YasuhiroTakemura <fnifhubi85h29bddi@gmail.com>
2025-03-08 11:19:24 +0900

    feat: 例外基盤クラスの作成

---
+++ EnglishApp.Domain/Exceptions/InputException.cs
@@ -0,0 +1,14 @@
+﻿using EnglishApp.Domain.Exceptions.Bases;
+
+namespace EnglishApp.Domain.Exceptions;
+
+public sealed class InputException : ExceptionBase
+{
+    public InputException() : base() { }
+
+    public InputException(string message) : base(message) { }
+
+    public InputException(string? message, Exception? innerException) : base(message, innerException) { }
+
+    public override ExceptionType ExceptionType => ExceptionType.Error;
+}
