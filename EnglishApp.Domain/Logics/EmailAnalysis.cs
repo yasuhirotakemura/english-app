bc905f1aa5c7d560b71951fe0c0b3ebae172fac4
YasuhiroTakemura <fnifhubi85h29bddi@gmail.com>
2025-03-09 10:03:42 +0900

    feat: Email解析クラスの実装

---
+++ EnglishApp.Domain/Logics/EmailAnalysis.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace EnglishApp.Domain.Logics;
+
+public static class EmailAnalysis
+{
+    public static bool IsValidEmail(string email)
+    {
+        if (String.IsNullOrWhiteSpace(email))
+        {
+            return false;
+        }
+
+        try
+        {
+            email = Regex.Replace(email, @"(@)(.+)$", DomainMapper,
+                                  RegexOptions.None, TimeSpan.FromMilliseconds(200));
+        }
+        catch (RegexMatchTimeoutException e)
+        {
+            return false;
+        }
+        catch (ArgumentException e)
+        {
+            return false;
+        }
+
+        try
+        {
+            return Regex.IsMatch(email,
+                @"^[^@\s]+@[^@\s]+\.[^@\s]+$",
+                RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+        }
+        catch (RegexMatchTimeoutException)
+        {
+            return false;
+        }
+    }
+
+    private static string DomainMapper(Match match)
+    {
+        IdnMapping idn = new();
+
+        string domainName = idn.GetAscii(match.Groups[2].Value);
+
+        return match.Groups[1].Value + domainName;
+    }
+}
