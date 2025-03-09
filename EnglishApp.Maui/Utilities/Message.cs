8f971322df34e910534b19f8bb8a10c93ccedff8
YasuhiroTakemura <fnifhubi85h29bddi@gmail.com>
2025-03-09 11:02:15 +0900

    feat: Messageクラスの追加

---
+++ EnglishApp.Maui/Utilities/Message.cs
@@ -0,0 +1,16 @@
+﻿using EnglishApp.Domain.Interfaces;
+
+namespace EnglishApp.Maui.Utilities;
+
+public sealed class Message : IMessage
+{
+    public Task Show(string title, string message, string cancel = "はい")
+    {
+        return Shell.Current.CurrentPage.DisplayAlert(title, message, cancel);
+    }
+
+    public Task<bool> ShowConfirm(string title, string message, string accept = "はい", string cancel = "いいえ")
+    {
+        return Shell.Current.CurrentPage.DisplayAlert(title, message, accept, cancel);
+    }
+}
