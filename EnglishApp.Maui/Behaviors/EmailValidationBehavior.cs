1a77711b7ea74a004793a85871f448f9529eaf8f
YasuhiroTakemura <fnifhubi85h29bddi@gmail.com>
2025-03-09 10:04:26 +0900

    Reapply "feat: Email解析クラスの実装"
    This reverts commit b2464fe702857299d8eedf36f9a795d310313f59.

---
+++ EnglishApp.Maui/Behaviors/EmailValidationBehavior.cs
@@ -0,0 +1,33 @@
+﻿using EnglishApp.Domain.Logics;
+
+namespace EnglishApp.Maui.Behaviors;
+
+public class EmailValidationBehavior : Behavior<Entry>
+{
+    protected override void OnAttachedTo(Entry entry)
+    {
+        base.OnAttachedTo(entry);
+        entry.Unfocused += this.OnEntryUnfocused;
+    }
+
+    protected override void OnDetachingFrom(Entry entry)
+    {
+        base.OnDetachingFrom(entry);
+        entry.Unfocused -= this.OnEntryUnfocused;
+    }
+
+    private async void OnEntryUnfocused(object? sender, FocusEventArgs e)
+    {
+        if(sender is not Entry entry)
+        {
+            return;
+        }
+
+        if(!EmailAnalysis.IsValidEmail(entry.Text) &&
+            Shell.Current is Shell currentShell &&
+            currentShell.CurrentPage is Page currentPage)
+        {
+            await currentPage.DisplayAlert("エラー", "無効なメールアドレスです。", "OK");
+        }
+    }
+}
