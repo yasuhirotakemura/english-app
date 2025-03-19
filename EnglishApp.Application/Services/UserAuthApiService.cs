f3997a7c446ef3eb40c5dbed33bcbb0978afe118
YasuhiroTakemura <fnifhubi85h29bddi@gmail.com>
2025-03-19 11:57:40 +0900

    feat: UserAuthApiServiceの実装

---
+++ EnglishApp.Application/Services/UserAuthApiService.cs
@@ -0,0 +1,17 @@
+﻿using EnglishApp.Application.Apis;
+using EnglishApp.Application.Dtos;
+using System.Net.Http.Json;
+
+namespace EnglishApp.Application.Services;
+
+public sealed class UserAuthApiService(HttpClient httpClient) : IUserAuthApiService
+{
+    private readonly HttpClient _httpClient = httpClient;
+
+    public async Task<bool> SignUpAsync(UserAuthSignUpRequest request)
+    {
+        HttpResponseMessage response = await this._httpClient.PostAsJsonAsync("http://10.0.2.2:5249/user/signup", request);
+
+        return response.IsSuccessStatusCode;
+    }
+}
