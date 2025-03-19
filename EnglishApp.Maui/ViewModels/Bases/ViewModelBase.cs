21dd5df4bb83578ab7159b516edab2a23d10067f
YasuhiroTakemura <fnifhubi85h29bddi@gmail.com>
2025-03-19 15:34:26 +0900

    refactor: ViewModelBaseのNavメソッドのアクセス就職誌の変更

--- EnglishApp.Maui/ViewModels/Bases/ViewModelBase.cs
+++ EnglishApp.Maui/ViewModels/Bases/ViewModelBase.cs
@@ -4,7 +4,7 @@ namespace EnglishApp.Maui.ViewModels.Bases;
 
 public abstract class ViewModelBase : ObservableObject
 {
-    public virtual Task NavigateToAsync(string route, Dictionary<string, object> parameter)
+    protected Task NavigateToAsync(string route, Dictionary<string, object> parameter)
     {
         return Shell.Current.GoToAsync(route, parameter);
     }
