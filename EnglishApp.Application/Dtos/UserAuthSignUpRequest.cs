522da0ffbf442ccae92ee910d432349018559712
YasuhiroTakemura <fnifhubi85h29bddi@gmail.com>
2025-03-19 17:49:27 +0900

    refactor: Dtoクラスにファクトリーメソッドを持たせる

--- EnglishApp.Application/Dtos/UserAuthSignUpRequest.cs
+++ EnglishApp.Application/Dtos/UserAuthSignUpRequest.cs
@@ -1,4 +1,6 @@
-﻿namespace EnglishApp.Application.Dtos;
+﻿using EnglishApp.Domain.ValueObjects;
+
+namespace EnglishApp.Application.Dtos;
 
 public class UserAuthSignUpRequest(string email,
                                           string passwordHash,
@@ -7,4 +9,9 @@ public class UserAuthSignUpRequest(string email,
     public string Email { get; } = email;
     public string PasswordHash { get; } = passwordHash;
     public string Salt { get; } = salt;
+
+    public static UserAuthSignUpRequest Create(string email, PasswordHash passwordHash)
+    {
+        return new UserAuthSignUpRequest(email, passwordHash.GetBase64Hash(), passwordHash.GetBase64Salt());
+    }
 }
