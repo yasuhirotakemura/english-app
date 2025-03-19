931f719937e7168265c1367e0c73d8f8134cee47
YasuhiroTakemura <fnifhubi85h29bddi@gmail.com>
2025-03-19 11:37:03 +0900

    feat: SignUp時のDtoの追加

---
+++ EnglishApp.Application/Dtos/UserAuthSignUpRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace EnglishApp.Application.Dtos;
+
+public sealed class UserAuthSignUpRequest(string email,
+                                          byte[] passwordHash,
+                                          byte[] salt)
+{
+    public string Email { get; } = email;
+    public byte[] PasswordHash { get; } = passwordHash;
+    public byte[] Salt { get; } = salt;
+}
