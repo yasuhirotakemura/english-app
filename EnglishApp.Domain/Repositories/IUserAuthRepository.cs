8b62aa1b67c6ab764bdc4daf1e16237024854639
YasuhiroTakemura <fnifhubi85h29bddi@gmail.com>
2025-03-19 10:45:15 +0900

    feat: UserAuthRepositoryの実装

--- EnglishApp.Domain/Repositories/IUserAuthRepository.cs
+++ EnglishApp.Domain/Repositories/IUserAuthRepository.cs
@@ -1,6 +1,8 @@
-﻿namespace EnglishApp.Domain.Repositories;
+﻿using EnglishApp.Domain.Entities;
+
+namespace EnglishApp.Domain.Repositories;
 
 public interface IUserAuthRepository
 {
-    public Task CreateUserAuth();
+    public Task CreateUserAuth(UserAuthEntity entity);
 }
---
+++ EnglishApp.Infrastructure/Repositories/UserAuthRepository.cs
@@ -0,0 +1,28 @@
+﻿using EnglishApp.Domain.Entities;
+using EnglishApp.Domain.Repositories;
+using EnglishApp.Infrastructure.Services;
+using Microsoft.Data.SqlClient;
+
+namespace EnglishApp.Infrastructure.Repositories;
+
+public sealed class UserAuthRepository(SqlServerService sqlServerService) : IUserAuthRepository
+{
+    private readonly SqlServerService _sqlServerService = sqlServerService;
+
+    public async Task CreateUserAuth(UserAuthEntity entity)
+    {
+        const string sql = @"
+            INSERT INTO UserAuth (UserId, Email, PasswordHash, Salt) 
+            VALUES (@UserId, @Email, @PasswordHash, @Salt);";
+
+        SqlParameter[] parameters =
+{
+            new("@UserId", entity.Id),
+            new("@Email", entity.Email),
+            new("@PasswordHash", entity.PasswordHash),
+            new("@Salt", entity.Salt)
+        };
+
+        await this._sqlServerService.ExecuteAsync(sql, parameters);
+    }
+}
