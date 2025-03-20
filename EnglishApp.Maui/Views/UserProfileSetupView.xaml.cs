87dafe0b6714f86107177dc6e1c6678b08e2e4c2
YasuhiroTakemura <fnifhubi85h29bddi@gmail.com>
2025-03-20 15:15:10 +0900

    feat: UserProfileSetupViewの作成

---
+++ EnglishApp.Maui/Views/UserProfileSetupView.xaml.cs
@@ -0,0 +1,13 @@
+﻿using EnglishApp.Maui.ViewModels;
+
+namespace EnglishApp.Maui.Views;
+
+public partial class UserProfileSetupView : ContentPage
+{
+	public UserProfileSetupView(UserProfileSetupViewModel userProfileSetupViewModel)
+	{
+		this.InitializeComponent();
+
+        this.BindingContext = userProfileSetupViewModel;
+	}
+}
