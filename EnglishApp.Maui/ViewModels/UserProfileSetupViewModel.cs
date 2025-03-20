2016e13aeebe7469545fc68b9f4bb808f261ae0f
YasuhiroTakemura <fnifhubi85h29bddi@gmail.com>
2025-03-20 15:15:29 +0900

    feat: UserProfileSetupViewModelの作成

---
+++ EnglishApp.Maui/ViewModels/UserProfileSetupViewModel.cs
@@ -0,0 +1,12 @@
+﻿using EnglishApp.Domain.Interfaces;
+using EnglishApp.Maui.ViewModels.Bases;
+
+namespace EnglishApp.Maui.ViewModels;
+
+public sealed class UserProfileSetupViewModel(IMessageService messageService) : ViewModelBase(messageService), IQueryAttributable
+{
+    public void ApplyQueryAttributes(IDictionary<string, object> query)
+    {
+        throw new NotImplementedException();
+    }
+}
