3cb1967febf9ed875a22497ec2d2e97a21e87122
YasuhiroTakemura <fnifhubi85h29bddi@gmail.com>
2025-03-23 13:28:57 +0900

    feat: UserProfileテーブルに関連するEntityの作成

---
+++ EnglishApp.Domain/Entities/UserProfileInsertEntity.cs
@@ -0,0 +1,20 @@
+﻿namespace EnglishApp.Domain.Entities;
+
+public sealed class UserProfileInsertEntity(int userId,
+                                           string nickName,
+                                           byte gender,
+                                           int gradeId,
+                                           int learningPurposeId,
+                                           int prefectureId,
+                                           DateTime? birthDate,
+                                           string? profileText)
+{
+    public int UserId { get; } = userId;
+    public string NickName { get; } = nickName;
+    public byte Gender { get; } = gender;
+    public int GradeId { get; } = gradeId;
+    public int LearningPurposeId { get; } = learningPurposeId;
+    public int PrefectureId { get; } = prefectureId;
+    public DateTime? BirthDate { get; } = birthDate;
+    public string? ProfileText { get; } = profileText;
+}
