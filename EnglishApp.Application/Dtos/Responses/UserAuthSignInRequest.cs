3f1b8999e2c8b8fa152940e47732c79225cdc2a2
YasuhiroTakemura <fnifhubi85h29bddi@gmail.com>
2025-03-25 05:23:20 +0900

    feat: SignIn時に必要なResponseクラスの作成

---
+++ EnglishApp.Application/Dtos/Responses/UserAuthSignInRequest.cs
@@ -0,0 +1,14 @@
+﻿using EnglishApp.Domain.ValueObjects;
+
+namespace EnglishApp.Application.Dtos.Responses;
+
+public sealed class UserAuthSignInRequest(string email, string passwordHash)
+{
+    public string Email { get; } = email;
+    public string PasswordHash { get; } = passwordHash;
+
+    public static UserAuthSignInRequest Create(string email, PasswordHash passwordHash)
+    {
+        return new UserAuthSignInRequest(email, passwordHash.GetBase64Hash());
+    }
+}
---
+++ EnglishApp.Application/Dtos/Responses/UserAuthSignInResponse.cs
@@ -0,0 +1,10 @@
+﻿using EnglishApp.Application.Dtos.Responses.Bases;
+
+namespace EnglishApp.Application.Dtos.Responses;
+
+public sealed class UserAuthSignInResponse(string message, int userId, string nickName, bool isEmailVerified) : ResponseBase(message)
+{
+    public int UserId { get; } = userId;
+    public string NickName { get; } = nickName;
+    public bool IsEmailVerified { get; } = isEmailVerified;
+}
